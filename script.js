62a54f8e0ccfa8fc660e61a9639974f01d5a4928
PhilipMa2 <ma_philip2000@hotmail.com>
2020-08-21 10:16:35 -0500

    Finish intro effect

---
+++ script.js
@@ -0,0 +1,22 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+const instance = new TypeIt('#typewriter', {
+  speed: 100,
+  lifelike: true,
+  loop: true
+})
+  .type("Hello World! I'm Philip Ma,")
+  .pause(1000)
+  .move(-4)
+  .delete(6)
+  .move(3)
+  .type(" Yueran")
+  .move(1)
+  .pause(1000)
+  .delete()
+  .type('an aspiring Programmer.')
+  .pause(1000)
+  .delete()
+  .go();
